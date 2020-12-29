11edbd77faed28ca66724e0d1bd74618c7b54393
Kevin Lam <61953680+KevinLam2980@users.noreply.github.com>
2020-12-28 23:44:57 -0800

    install axios dependency

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react'
+import {Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>haroot</h1>
+      
     </div>
   );
 }
